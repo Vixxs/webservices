5a54f39e1f76b7d94975f488728993a2575f4af3
Tom Guastapaglia <tomguasta@yahoo.fr>
2024-04-27 00:20:21 +0200

    feat(hal):json+hal + get movies for one category

---
+++ movies/src/movies/hal.category.interceptor.ts
@@ -0,0 +1,33 @@
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Category } from './entities/category.entity';
+
+export class HalCategoryInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (Array.isArray(data)) {
+          // If data is an array (e.g., a list of movies), add links to each item
+          return data.map((item) => this.addLinks(item));
+        } else {
+          // If data is a single object, add links to it
+          return this.addLinks(data);
+        }
+      }),
+    );
+  }
+
+  private addLinks(category: Category): any {
+    const baseUrl = '/api/categories';
+    return {
+      ...category,
+      _links: {
+        self: { href: `${baseUrl}/${category.id}` },
+        update: { href: `${baseUrl}/${category.id}`, method: 'PUT' },
+        delete: { href: `${baseUrl}/${category.id}`, method: 'DELETE' },
+        categories: { href: `${baseUrl}/${category.id}/categories` },
+      },
+    };
+  }
+}
